fb10e61fb3835b1ad198edd5cc6638e253e493e7
jaumefv981276345 <jaumefuster@paucasesnovescifp.cat>
2024-06-08 15:03:36 +0200

    Add files via upload

---
+++ guio_4.sql
@@ -0,0 +1,19 @@
+
+DROP PROCEDURE IF EXISTS pitxixi;
+
+DELIMITER $$
+
+create procedure pitxixi()
+begin
+select ju.nom as nom_jugador, eq.nom as nom_equip, count(es.id_esdeveniment) as num_gols from jugador ju
+    join equip eq using (id_equip)
+    join esdeveniment es on ju.id_jugador = es.id_jugador
+where es.tipus = 'G'
+group by ju.id_jugador, ju.nom, eq.nom
+order by num_gols desc
+limit 1;
+END$$
+
+DELIMITER ;
+
+CALL pitxixi();
