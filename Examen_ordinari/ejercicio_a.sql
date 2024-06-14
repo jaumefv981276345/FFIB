aa395fd7ab51a2995a67d4d7bd4a04daabfb0f91
moliverbes <167178000+moliverbes@users.noreply.github.com>
2024-06-14 23:56:40 +0200

    Update ejercicio_a.sql - corregit

--- Examen_ordinari/ejercicio_a.sql
+++ Examen_ordinari/ejercicio_a.sql
@@ -1,13 +1,13 @@
 
 DELIMITER //
 
-CREATE PROCEDURE jugadors_convocats(in nom_equip varchar(100),in partit smallint)
+CREATE PROCEDURE jugadors_convocats(in nom_equip varchar(100),in partit smallint)  -- no necessitam 2 paràmetres. De fet no en necessitam cap.
 BEGIN
 	select count(id_jugador) from convocat co
 	join jugador using (id_jugador)
 	join equip eq using (id_equip)
-	join partit pa using (id_partit)
-	where id_jugador = null and id_patit = partit and eq.nom = nom_equip;
+	join partit pa using (id_partit) -- partit no ens fa falta
+	where id_jugador = null and id_patit = partit and eq.nom = nom_equip; --id_jugador no pot ser mai null.
     
 END//
 
