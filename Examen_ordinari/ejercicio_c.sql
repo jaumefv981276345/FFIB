ab23f40465288d4672272439b4ce9535b6412b96
moliverbes <167178000+moliverbes@users.noreply.github.com>
2024-06-15 00:05:22 +0200

    Update ejercicio_c.sql - corregit

--- Examen_ordinari/ejercicio_c.sql
+++ Examen_ordinari/ejercicio_c.sql
@@ -7,12 +7,18 @@ BEFORE INSERT on esdeveniment
 
 BEGIN
 declare tipus smallint;
+-- Faltaria IF new.tipus = 'CV'
 set tipus = (
 	select titular from convocat
-	where id_jugador = new.id_jugador);
+	where id_jugador = new.id_jugador);  -- També hauries d'afegir l'id_partit a la condició
 
 	if tipus = 1 then
-    send mesaje = ('Esta convocat com a suplent')
+    send mesaje = ('Esta convocat com a suplent')   
+	-- Posar-lo com a suplent significa el següent
+   ALTER TABLE convocat
+	SET titular = 0
+	WHERE id_jugador = new.id_jugador AND id_partit = new.id_partit;
+		
     elseif tipus = 0 then
     insert into 'convocat' (titular)
     values (1)
